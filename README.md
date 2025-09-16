b7dbcedcf247a274263c2af5584d7271c49aeba8
greg-chinnici <chinnici@chapman.edu>
2025-09-15 19:13:24 -0700

    generate color settings

---
+++ README.md
@@ -0,0 +1,9 @@
+This will be a tool for turning a single image into a 3d printable poster
+Inspo: https://www.youtube.com/watch?v=FKd9X-wl8Rg
+
+Steps:
+1. quantize image
+2. vectorize new image
+3. create instuctions for user to use in Fusion
+4. manually position in fusion then export to print
+5. create color swaps at differtn layers in prusa slicer
---
+++ toolset/settings.py
@@ -0,0 +1,29 @@
+import json
+
+def CreateDefaults():
+    defaults = {
+        "colors": ["#FFFFFF", "#FF0000", "#00FF00", "#0000FF", "#000000"],
+    }
+
+    with open("settings.json", "w") as f:
+        json.dump(defaults, f, indent=4)
+
+def OverrideColors(colors:list[str]):
+    try:
+        with open("settings.json", "r+") as f:
+            settings = json.load(f)
+            settings["colors"] = colors
+            f.seek(0)             
+            json.dump(settings, f, indent=4)
+            f.truncate() 
+    except FileNotFoundError:
+        print("Settings file not found. Using default colors.")
+
+def LoadSettings():
+    try:
+        with open("settings.json", "r") as f:
+            settings = json.load(f)
+            return settings
+    except FileNotFoundError:
+        print("Settings file not found.")
+        return None
