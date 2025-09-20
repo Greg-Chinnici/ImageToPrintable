e71d4191b7e1b81d4de5bdbbd72a7623da149715
greg-chinnici <chinnici@chapman.edu>
2025-09-19 21:58:22 -0700

    add example images to readme

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
+
 This will be a tool for turning a single image into a 3d printable poster
 Inspo: https://www.youtube.com/watch?v=FKd9X-wl8Rg
+
+<table>
+  <tr>
+    <td><img src="examples/test.png" width="300"/></td>
+    <td><img src="examples/test_q.png" width="300"/></td>
+    <td><img src="examples/test_q_pal.png" width="300"/></td>
+  </tr>
+  <tr>
+    <td align="center">Original Image</td>
+    <td align="center">LAB Color Quantization</td>
+    <td align="center">LAB with Pallete</td>
+  </tr>
+</table>
 
 Steps:
 - [x] quantize image using LAB color space
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #uv run main.py -i colortest.png -p '255 0 0 , #00ff00 , #777777'
-uv run main.py -i test.png -p '#ffffff , #000000 , 200 130 60' 
-uv run main.py -i test.png -p '#ffffff , #000000 , 200 130 60 , #444444' -o test2_q.png --AddPalette --Image
-uv run main.py -i test.png -p '#ffffff , #000000 , 200 130 60 ,#333333' -o test3_q.png --AddPalette --Image
-
+#uv run main.py -i test.png -p '#ffffff , #000000 , 200 130 60' 
+#uv run main.py -i test.png -p '#ffffff , #000000 , 200 130 60 , #444444' -o test2_q.png --AddPalette --Image
+uv run main.py -i test.png -p '#ffffff , #000000 , 200 130 60 ,#333333' -o test_q_pal.png --AddPalette --Image
+uv run main.py -i test.png -p '#ffffff , #000000 , 200 130 60 ,#333333' -o test_q.png --Image
 
 #rm $(grep 'quantized.png')
