c93b201576e290be479209645b2c5899797807ee
greg-chinnici <chinnici@chapman.edu>
2025-09-21 17:12:54 -0700

    sort bitmap files by L from rgb ot lab

--- toolset/extrude.py
+++ toolset/extrude.py
@@ -1,18 +1,29 @@
 import os 
 from PIL import Image
+from quantize import rgb_to_lab
+
 
 class Extruder:
     def __init__(self, bitmaps_folder, svg_folder, layer_height_mm=10):
-        pass
+        self.bitmaps_folder = bitmaps_folder
+        self.svg_folder = svg_folder
+        self.layer_height_mm = layer_height_mm
+        
 
     def extrude_with_bitmap(self):
-        bitmap_files = os.getfiles(os.path.join(self.bitmaps_folder))
+        bitmap_files = [f for f in os.listdir(self.bitmaps_folder) if f.endswith('.png')]
         rgb_colors = [f.replace('.png','').split('_')[1:] for f in bitmap_files]
         color_to_file = dict(zip(rgb_colors , bitmap_files))
-        # sort rgb to lab based on L to find height
 
-        for i, f in enumerate(bitmap_files):
-            img = Image.open(f)
-            color = rgb_colors[i]
+        lab_to_rgb = {tuple(rgb_to_lab([int(c) for c in rgb])): rgb for rgb in rgb_colors}
+        lab_colors = sorted(lab_to_rgb.keys(), key=lambda x: x[0])
+
+        for i, c in enumerate(lab_colors):
+            rgb_color = lab_to_rgb(c)
+            img_path = os.path.join(self.bitmaps_folder, color_to_file[rgb_color])
+            img = Image.open(img_path)
+            
+            # read in the bitmap and extrude it based on 'i'
+
         pass
     
--- toolset/quantize.py
+++ toolset/quantize.py
@@ -3,7 +3,40 @@ from PIL import Image, ImageDraw
 import os
 from sklearn.metrics import pairwise_distances_argmin
 
-
+    
+# rgb list is 3 elements of range 0-255
+def rgb_to_lab(self, rgb: tuple[int, int, int]) -> list[float]:
+    r, g, b = [c / 255.0 for c in rgb]
+    
+    def gamma_expand(c):
+        return c / 12.92 if c <= 0.04045 else ((c + 0.055) / 1.055) ** 2.4
+    
+    linear_r, linear_g, linear_b = [gamma_expand(c) for c in (r, g, b)]
+    
+    # sRGB to XYZ transformation matrix (D65)
+    X = linear_r * 0.4124564 + linear_g * 0.3575761 + linear_b * 0.1804375
+    Y = linear_r * 0.2126729 + linear_g * 0.7151522 + linear_b * 0.0721750
+    Z = linear_r * 0.0193339 + linear_g * 0.1191920 + linear_b * 0.9503041
+    
+    # D65 reference white
+    Xn, Yn, Zn = 95.047, 100.000, 108.883
+    x = X / Xn * 100  
+    y = Y / Yn * 100
+    z = Z / Zn * 100
+    
+    delta = 6 / 29
+    def f(t):
+        return t ** (1/3) if t > delta**3 else (t / (3 * delta**2)) + (4 / 29)
+    
+    fx = f(x)
+    fy = f(y) 
+    fz = f(z)
+    
+    L = 116 * fy - 16
+    a = 500 * (fx - fy)
+    b = 200 * (fy - fz)
+    
+    return [L, a, b]
 
 class Quantizer:
     def __init__(self, color_list, settings_file="settings.json"):
@@ -100,37 +133,3 @@ class Quantizer:
     def hex_to_rgb(self, hex_color:str) -> tuple[int,int,int]:
         hex_color = hex_color.lstrip('#')
         return [int(hex_color[i:i+2], 16) for i in (0, 2, 4)]
-    
-    # rgb list is 3 elements of range 0-255
-    def rgb_to_lab(self, rgb: tuple[int, int, int]) -> list[float]:
-        r, g, b = [c / 255.0 for c in rgb]
-        
-        def gamma_expand(c):
-            return c / 12.92 if c <= 0.04045 else ((c + 0.055) / 1.055) ** 2.4
-        
-        linear_r, linear_g, linear_b = [gamma_expand(c) for c in (r, g, b)]
-        
-        # sRGB to XYZ transformation matrix (D65)
-        X = linear_r * 0.4124564 + linear_g * 0.3575761 + linear_b * 0.1804375
-        Y = linear_r * 0.2126729 + linear_g * 0.7151522 + linear_b * 0.0721750
-        Z = linear_r * 0.0193339 + linear_g * 0.1191920 + linear_b * 0.9503041
-        
-        # D65 reference white
-        Xn, Yn, Zn = 95.047, 100.000, 108.883
-        x = X / Xn * 100  
-        y = Y / Yn * 100
-        z = Z / Zn * 100
-        
-        delta = 6 / 29
-        def f(t):
-            return t ** (1/3) if t > delta**3 else (t / (3 * delta**2)) + (4 / 29)
-        
-        fx = f(x)
-        fy = f(y) 
-        fz = f(z)
-        
-        L = 116 * fy - 16
-        a = 500 * (fx - fy)
-        b = 200 * (fy - fz)
-        
-        return [L, a, b]
